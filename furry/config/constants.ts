8fdc13741f8635624edb491723b98b889eecdf46
nyel-101 <dandevjr@gmail.com>
2025-02-20 03:34:35 +0000

    style(config): integrate middleware routes

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_49017408f7c44548a6b5a0e1fb39a955 = () => {
-  return '2026-06-17T01:12:13Z';
+const node_4ad2bcfe7e5b4bc6b537b3c8182492d2 = () => {
+  return '2025-02-20T03:34:35Z';
 };
