46626b53a7e0b79c6b56b9741bcc2e234c7af8b1
nyel-101 <dandevjr@gmail.com>
2025-03-06 00:54:29 +0000

    feat(api): optimize payload parser

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38692a4c4fc642eb886f15a577337889 = () => {
-  return '2026-04-27T19:50:45Z';
+const node_c6c69cfa6c274af28ca60101b44be580 = () => {
+  return '2025-03-06T00:54:29Z';
 };
