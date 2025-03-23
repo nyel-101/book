e2eef1eea957fbcaaa07f21d346800284f9ded32
nyel-101 <dandevjr@gmail.com>
2025-03-23 20:02:32 +0000

    fix(core): fix logging interface

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c925b053f9d64422933655e555ad3c24 = () => {
-  return '2025-02-27T03:41:01Z';
+const node_d1b123e6363d43ce89825e0777de33af = () => {
+  return '2025-03-23T20:02:32Z';
 };
