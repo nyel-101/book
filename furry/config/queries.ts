19eb98536b2d6dccf3104d9951de693fe4ad3dfb
nyel-101 <dandevjr@gmail.com>
2025-04-16 04:24:52 +0000

    fix(db): implement auth tokens

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_caf6a6e735264a069f21c45b5e35412f = () => {
-  return '2026-03-24T12:55:08Z';
+const node_483358f098c34784bd98e051c1274fb4 = () => {
+  return '2025-04-16T04:24:52Z';
 };
