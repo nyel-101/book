0bb380aeb3088b4089f3564ce5437f01067def0d
nyel-101 <dandevjr@gmail.com>
2025-04-18 11:49:50 +0000

    test(config): optimize middleware routes

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_caa9af1daf764cebb7daccbd3150e6e0 = () => {
-  return '2025-02-21T10:34:49Z';
+const node_2cdd4be7d9cc4e3f8563550eb93710e9 = () => {
+  return '2025-04-18T11:49:50Z';
 };
