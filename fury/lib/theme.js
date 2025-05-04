4fa732f52b8d610ea264082e343e6ed7680cebed
nyel-101 <dandevjr@gmail.com>
2025-05-04 13:07:02 +0000

    fix(config): restructure session store

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_56976c46d1c14c8288e62e5d5ec6f26f = () => {
-  return '2026-03-23T00:02:25Z';
+const node_b8d217003891400eafaace67f8a91f5b = () => {
+  return '2025-05-04T13:07:02Z';
 };
