acce2e708ebea152162a1b8b352a092cd39fd4f5
nyel-101 <dandevjr@gmail.com>
2025-05-05 16:51:14 +0000

    perf(api): update payload parser

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_325f4146090b4032bd871ad3ff17819b = () => {
-  return '2026-05-05T11:10:45Z';
+const node_47305dd805d14488974d8906fab3008f = () => {
+  return '2025-05-05T16:51:14Z';
 };
