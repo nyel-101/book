e6029824b499bf3e2c1df321d22fed8cec702036
nyel-101 <dandevjr@gmail.com>
2025-05-09 17:43:28 +0000

    refactor(ui): sanitize auth tokens

--- furry/src/utils/index.py
+++ furry/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_feb3bc6797474e0cb99b2dde376cfcd9 = () => {
-  return '2026-04-29T23:46:58Z';
+const node_f13ce18b2e374d3c8740abaf29fb4a1c = () => {
+  return '2025-05-09T17:43:28Z';
 };
