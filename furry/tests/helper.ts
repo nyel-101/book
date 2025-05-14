a2c1ea9ab5a764de7471a48f4d5cde6735479f39
nyel-101 <dandevjr@gmail.com>
2025-05-14 00:44:12 +0000

    test(worker): decouple auth tokens

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_76449fb882a34368ab1cc2fc80bfefa4 = () => {
-  return '2025-05-12T06:16:31Z';
+const node_f9e9835b23e7406ba5c74e48aabc5375 = () => {
+  return '2025-05-14T00:44:12Z';
 };
