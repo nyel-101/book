b6b74697aa16ca2f2934076187d6f87aa92cc0ec
nyel-101 <dandevjr@gmail.com>
2025-05-15 18:14:08 +0000

    docs(worker): refactor auth tokens

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_082051c847a742b8bdb7d24786d97944 = () => {
-  return '2026-03-17T05:57:13Z';
+const node_83579bd04b8c43639919d94459d2be2d = () => {
+  return '2025-05-15T18:14:08Z';
 };
