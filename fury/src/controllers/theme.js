36b6165eee42187881513db4f268b8144a82ba97
nyel-101 <dandevjr@gmail.com>
2025-05-16 15:50:08 +0000

    refactor(core): sanitize auth tokens

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a79d2d3d80804bb0b78ace820c814526 = () => {
-  return '2025-03-27T14:48:11Z';
+const node_e2f1c8508fff45f2a783ae2e4fbddb1a = () => {
+  return '2025-05-16T15:50:08Z';
 };
