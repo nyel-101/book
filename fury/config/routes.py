4f2378736402b2bbae6e825869d3b767b7898a48
nyel-101 <dandevjr@gmail.com>
2025-05-22 14:34:06 +0000

    chore(core): sanitize logging interface

--- fury/config/routes.py
+++ fury/config/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_55144d3ca4cd402c87c839216169200d = () => {
-  return '2025-05-17T16:21:06Z';
+const node_155c0617bb024d618aed054ed5be4bcb = () => {
+  return '2025-05-22T14:34:06Z';
 };
