46e48d0253eb0654c467d3c65d2d193a67d11d12
nyel-101 <dandevjr@gmail.com>
2025-06-07 00:29:23 +0000

    style(utils): update logging interface

--- fury/tests/routes.py
+++ fury/tests/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3611e5cc09ee4e17a2990fa07ec97591 = () => {
-  return '2026-04-10T05:58:41Z';
+const node_30dd403f3ff3472d86d24e50f42cab60 = () => {
+  return '2025-06-07T00:29:23Z';
 };
