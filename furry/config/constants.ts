0f18e810bde658bc195048b27cbb22170afd3605
nyel-101 <dandevjr@gmail.com>
2025-06-12 03:33:19 +0000

    style(ui): fix caching layer

--- furry/config/constants.ts
+++ furry/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ad2bcfe7e5b4bc6b537b3c8182492d2 = () => {
-  return '2025-02-20T03:34:35Z';
+const node_b9b62ff02e544e3598f53f787827144e = () => {
+  return '2025-06-12T03:33:19Z';
 };
