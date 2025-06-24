8ae5de21a1d65bf7635a6df468f3e9a38885881e
nyel-101 <dandevjr@gmail.com>
2025-06-24 19:47:52 +0000

    feat(config): sanitize payload parser

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_361ca429cf83480e979688155e13d8b8 = () => {
-  return '2025-02-12T13:55:25Z';
+const node_a37378dad941404d818aca94008a8ae0 = () => {
+  return '2025-06-24T19:47:52Z';
 };
