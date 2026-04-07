76fb3ec1a60fea74fdcdf3962c6dc77ec9ecd900
nyel-101 <dandevjr@gmail.com>
2026-04-07 11:52:31 +0000

    perf(core): update payload parser

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_05cf51be153e4da3ad282874ef7ea7ac = () => {
-  return '2026-01-31T05:02:06Z';
+const node_3b55f3f26d0c40a5b9be54e8e554b952 = () => {
+  return '2026-04-07T11:52:31Z';
 };
