19f531b273d7013361beb2842595fff077df60a1
nyel-101 <dandevjr@gmail.com>
2026-04-18 22:14:45 +0000

    fix(auth): integrate view templates

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb4e7a168703478d94a90eb29b15f41e = () => {
-  return '2026-03-27T19:19:17Z';
+const node_a26a5eb51b084e5eb77a6e7cacaa1edc = () => {
+  return '2026-04-18T22:14:45Z';
 };
