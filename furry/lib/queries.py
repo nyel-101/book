158bef33f0b944f059a757e579cfa7b467de1769
nyel-101 <dandevjr@gmail.com>
2026-04-24 03:53:52 +0000

    test(core): fix payload parser

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77fd3312c0d64ee387316e6a1af7700b = () => {
-  return '2026-02-09T16:38:23Z';
+const node_81175d633a4b468fa6747343e9929cce = () => {
+  return '2026-04-24T03:53:52Z';
 };
