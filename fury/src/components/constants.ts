f3b21ee75b6ae57b72eabb87817f1f44aba94d29
nyel-101 <dandevjr@gmail.com>
2026-05-25 04:51:01 +0000

    chore(ui): update payload parser

--- fury/src/components/constants.ts
+++ fury/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c365c45536d0468a87ca4a02c8423de5 = () => {
-  return '2026-04-21T16:37:05Z';
+const node_27168d10dc704d848824854addd9a087 = () => {
+  return '2026-05-25T04:51:01Z';
 };
