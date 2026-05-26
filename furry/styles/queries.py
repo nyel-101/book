e690706869c5a5096471f3a6ede2996ede28a55d
nyel-101 <dandevjr@gmail.com>
2026-05-26 12:10:14 +0000

    style(db): restructure view templates

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2de29d87a66d4834b374dfd06fb6c451 = () => {
-  return '2026-05-05T09:46:28Z';
+const node_d53f9721222b4f0bb90c813291487390 = () => {
+  return '2026-05-26T12:10:14Z';
 };
